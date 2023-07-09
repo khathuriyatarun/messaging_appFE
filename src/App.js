d18f18a0d6a121c8055feab8e899e7257e50f688
khathuriyatarun <tarunkhathuriya@gmail.com>
2023-07-10 00:17:11 +0530

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import './App.css';
-import DisplayPicture from './components/DisplayPicture';
-import { data } from './data/user';
+import './App.css';;
 
 function App() {
   return (
     <div className="App">
-      {
-        data.map((user) => {
-          return (
-            <DisplayPicture width={49} height={49} user={user} />
-          )
-        })
-      }
-      
-      <DisplayPicture width={200} height={200} />
     </div>
   );
 }
